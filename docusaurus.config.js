c98ab174e391df8bdacf1ff005461c7492ce393d
weihu <weihu@apache.org>
2025-02-28 17:49:21 +0800

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://weihubeats.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/weihubeats/',
+  baseUrl: '/weihubeats-website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
